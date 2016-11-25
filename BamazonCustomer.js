d9d4f71d970aa949a5e9697a510e31d464872596
Michelle Hettinger <Michelle Hettinger>
2016-11-25 02:09:53 -0500

    filled out readme, deleted some files

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -1,33 +1,25 @@
-// 5. Then create a Node application called `BamazonCustomer.js`. Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
-
-// 6. The app should then prompt users with two messages. 
-// 	* The first should ask them the ID of the product they would like to buy. 
-// 	* The second message should ask how many units of the product they would like to buy.
-
-// 7. Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request. 
-// 	* If not, the app should log a phrase like `Insufficient quantity!`, and then prevent the order from going through.
-
-// 8. However, if your store *does* have enough of the product, you should fulfill the customer's order. 
-// 	* This means updating the SQL database to reflect the remaining quantity.
-// 	* Once the update goes through, show the customer the total cost of their purchase.
-
 var inquirer = require("inquirer");
 var mysql = require("mysql");
 
+
+// MySQL connection details
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
-    user: "root", //Your username
-    password: "s0nyd0g", //Your password
+    user: "root",
+    password: "s0nyd0g",
     database: "bamazon"
 });
 
+// Establish connection to database
 connection.connect(function(err) {
     if (err) throw err;
 });
 
+// Perform a get query to retrieve the stores items.
 connection.query('SELECT * FROM products', function(err, res) {
     if (err) throw err;
+    
     console.log("Item # | Product -- Department -- Price -- Quantity ");
 
     //Loop through all the rows and print out their column values
@@ -44,9 +36,13 @@ connection.query('SELECT * FROM products', function(err, res) {
 });
 
 var promptUser = function(){
+
+    // Prompt the user with a message
 	inquirer.prompt([{
 		name: "Item_ID",
 		message: "Enter the ID of the item you wish to purchase.",
+
+        // Make sure that they typed in a number and not a letter
 		validate: function(value){
             if (isNaN(value) == false) {
                 return true;
@@ -56,8 +52,12 @@ var promptUser = function(){
             }
 		}
 	},{
+
+        // After the first prompt, do another
         name: "userQuantity",
         message: "How many would you like to buy?",
+
+        // And validate they typed in a number
         validate: function(value){
             if (isNaN(value) == false) {
                 return true;
@@ -65,8 +65,11 @@ var promptUser = function(){
             else {
                 return false;
             }
-        }        
+        }
+        // After the series of prompts
     }]).then(function(answers){
+
+            // Set the userinput to currentItem and currentAmount
     		var currentItem = answers.Item_ID;
     		var currentAmount = answers.userQuantity;
 
@@ -75,23 +78,32 @@ var promptUser = function(){
             connection.query('SELECT * FROM products WHERE ?',{
                 Item_ID: answers.Item_ID
             },function(err, res){
+
+                //If the amount requested is greater than the amount in stock.
                 if (currentAmount > res[0].Stock_Quantity){
                     console.log("You cannot buy that many!");
+
+                    // Back to prompt
                     promptUser();
                 }
+                // Otherwise they may buy it
                 else { 
                     console.log("You can buy it!");
 
+                    // Calculate the new quantity to update in the database
                     var newQuantity = (res[0].Stock_Quantity - currentAmount);
                     var totalCost = res[0].Price*currentAmount;
 
+                    // Update the quantity
                     connection.query('UPDATE products SET ? WHERE ?',[{
                         Stock_Quantity: newQuantity
                     },{
                         Item_ID: currentItem
                     }], function(err, res){
                         console.log("You were charged $" + totalCost);
-                        connection.end();
+
+                        // Back to the prompt
+                        promptUser();
                     });
                 }
             })
--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # Bamazon
 Database project to create a node app that allows purchasing of items from a database.
 
-NPM Inquirer: https://www.npmjs.com/package/inquirer
-NPM MySQL: https://www.npmjs.com/package/mysql
+Instructions:
+1) Clone or download the git repository.
+2) Install Node.js with NPM ( https://nodejs.org/en/download/ ).
+3) Install MySQL ( https://www.mysql.com/downloads/ ). Read online how to set up a local connection.
+4) Open BamazonCustomer.js in your editor and fill out your mysql username and password.
+5) Open your MySQL editor ( http://www.mysql.com/products/workbench/ for Windows) and run the bamazon.sql file.
+6) Run the command ( npm install ) in the terminal.
+7) Run the command ( node BamazonCustomer.js ).
+8) Follow the prompts to purchase products! Ctrl+C (or cmd+C) to exit the app.
